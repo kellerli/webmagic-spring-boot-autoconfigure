394cd02949aca3a29a2b615d1de8db8b6c40b6a3
andy429006 <xianfeng.xiong@caigin.com>
2017-09-13 09:13:29 +0800

    修改实现类

--- src/main/java/cn/wanru/webmagic/pageprocessor/ClassBasePageablePageProcessor.java
+++ src/main/java/cn/wanru/webmagic/pageprocessor/ClassBasePageablePageProcessor.java
@@ -1,15 +1,23 @@
 package cn.wanru.webmagic.pageprocessor;
 
+import cn.wanru.webmagic.Pageable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 
+import java.util.Collection;
+
 import static cn.wanru.webmagic.PageUtil.getSupportClass;
+import static cn.wanru.webmagic.PageUtil.setData;
 
 /**
  * @author xxf
- * @date 17-9-11
+ * @date 17-9-10
  */
-public abstract class ClassBasePageablePageProcessor
+public abstract class ClassBasePageablePageProcessor<T>
         extends PageablePageProcessor {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     private Class<?> supportClass;
 
@@ -19,8 +27,31 @@ public abstract class ClassBasePageablePageProcessor
 
     @Override
     public boolean support(Page page) {
-        return getSupportClass(page.getRequest())
-                == supportClass;
+        Class<?> clazz = getSupportClass(page.getRequest());
+        return clazz == supportClass;
     }
 
+    @Override
+    protected void processPageAndUpdatePageable(Page page,Pageable pageable) {
+        T data = parsePage(page,pageable);
+        setData(data, page);
+        updatePageable(data,pageable);
+        if (log.isDebugEnabled()) {
+            int size = data instanceof Collection
+                    ? ((Collection) data).size() : 1;
+
+            log.debug("url[{}] crawl success,size=[{}]",
+                    page.getRequest().getUrl(), size);
+        }
+    }
+
+    /**
+     * 解析 Page 返回结果
+     * @param page
+     * @param pageable
+     * @return
+     */
+    protected abstract T parsePage(Page page, Pageable pageable);
+
+    protected abstract void updatePageable(T data,Pageable pageable);
 }
--- src/main/java/cn/wanru/webmagic/pipeline/ClassBasePipeline.java
+++ src/main/java/cn/wanru/webmagic/pipeline/ClassBasePipeline.java
@@ -1,14 +1,16 @@
 package cn.wanru.webmagic.pipeline;
 
 import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
 
+import static cn.wanru.webmagic.PageUtil.getData;
 import static cn.wanru.webmagic.PageUtil.getSupportClass;
 
 /**
  * @author xxf
  * @since 2017/9/12
  */
-public abstract class ClassBasePipeline implements SupportablePipeline {
+public abstract class ClassBasePipeline<T> implements SupportablePipeline {
 
     private Class<?> supportClass;
 
@@ -25,5 +27,12 @@ public abstract class ClassBasePipeline implements SupportablePipeline {
     public int getOrder() {
         return 0;
     }
+
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+        processResult(getData(resultItems));
+    }
+
+    protected abstract void processResult(T result);
 
 }
